719854c5ba6c29cdf84cf0f4a7d69a67bf043d4d
sonakshiyadv <sonakshiyd29@gmail.com>
2025-10-26 00:48:45 +0530

    Add frontend folder contents

---
+++ frontend/src/App.jsx
@@ -0,0 +1,30 @@
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      hello
+    </>
+  )
+}
+import { Outlet } from "react-router-dom";
+import Navigation from "./pages/Auth/Navigation";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const App = () => {
+  return (
+    <>
+      <ToastContainer />
+      <Navigation />
+      <main className="py-3">
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default App;
+
+
+export default App
